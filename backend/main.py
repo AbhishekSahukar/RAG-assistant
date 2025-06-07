69544a2ca53354cdfe98ef3945d6c85544047ab7
Abhishek SS <Sahukarabhi.10@gmail.com>
2025-06-07 23:23:50 +0530

    Backend changes

--- backend/main.py
+++ backend/main.py
@@ -3,10 +3,20 @@ from backend.chat_router import chat_router
 from backend.upload_router import upload_router
 from backend.status_router import status_router
 from backend.clear_router import clear_router
-
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
 from fastapi import FastAPI
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://rag-assistant-s2hj.onrender.com"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Include routers
 app.include_router(chat_router, prefix="/chat", tags=["Chat"])
