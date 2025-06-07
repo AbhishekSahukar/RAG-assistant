83bd5ce3a5409cdaec5f34ffb4de18f8d406d2d7
Abhishek SS <Sahukarabhi.10@gmail.com>
2025-06-07 23:56:48 +0530

    Backend changes

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://rag-assistant-s2hj.onrender.com"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
