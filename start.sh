a41de70100cece64ebb074ac685f8b5b65110dd8
Abhishek SS <Sahukarabhi.10@gmail.com>
2025-06-08 16:08:47 +0530

    New version

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Start FastAPI on port 8000
+echo " Starting FastAPI..."
 uvicorn backend.main:app --host 0.0.0.0 --port 8000 &
 
-# Start Streamlit on port 8501
+echo " Starting Streamlit..."
 streamlit run frontend/app.py \
   --server.port=8501 \
   --server.address=0.0.0.0
